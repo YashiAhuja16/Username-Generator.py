ba7a598a1bc32f1d2d4f938a378191a9bf68db52
YashiAhuja16 <yashi.ahuja16@gmail.com>
2025-07-01 19:22:06 -0700

    Update Username Generator.py

--- Username Generator.py
+++ Username Generator.py
@@ -16,7 +16,7 @@ while True :
         comb2 = str (z) + random.choice (p) + random.choice (y)
         comb3 = p2[:2]  + random.choice (y) + p1 [2:]
         comb4 = random.choice (p) + random.choice (y) + str (z)
-        comb5 = p2[:3] + p1[-2:] + str (z) 
+        comb5 = p2[::3] + p1[::-2] + str (z) 
 
         # final output 
         final_com = [comb1, comb2, comb3, comb4, comb5]
